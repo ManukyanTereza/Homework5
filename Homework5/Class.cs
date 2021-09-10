556d5293bdb483121f4ecd99b3ec511dcf10599b
User <User@WINDOWS-8CK325N>
2021-09-10 17:14:02 +0400

    Add project files.

---
+++ Homework5/Class.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Homework5
+{
+
+   public class Person
+    {
+        public int age;
+        public string  gender;
+        public string name;
+        public bool ex_age(Person person)
+        {
+            if (person.age > 0 && person.age <= 100) return true;
+            return false;
+        }
+        public Person(int age, string gender,string name)
+        {
+            this.age = age;
+            this.gender = gender;
+            this.name = name;
+        }
+
+        public virtual void print()
+        {
+            Console.WriteLine(name +" "+"is a person");
+            Console.ReadLine();
+        }
+    };
+
+          class Doctor : Person
+        {
+            public Doctor(int age, string gender,string name) : base(age, gender,name) { }
+        
+            public override void print()
+            {
+                Console.WriteLine( name+" " +" is a doctor");
+                Console.ReadLine();
+            }
+        };
+        class Singer : Person
+        {
+        public Singer(int age, string gender,string name) : base(age, gender,name) { }
+        public override void print()
+            {
+                Console.WriteLine(name+" "+"is  a singer");
+                Console.ReadLine();
+            }
+        };
+         class Programmer : Person
+        {
+        public Programmer(int age, string gender,string name) : base(age, gender,name) { }
+        public override void print()
+            {
+                Console.WriteLine(name+" "+"is a programmer");
+                Console.ReadLine();
+            }
+        };
+    }
+
+
---
+++ Homework5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person obj1 = new Person(13, "male", "Noy");
+            obj1.print();
+            Singer obj2 = new Singer(25, "female", "Jane");
+            obj2.print();
+            Programmer obj3 = new Programmer(18, "male", "Leo");
+            obj3.print();
+            Doctor obj4 = new Doctor(150, "male", "Edvard");
+            try
+            {
+                ex_age(obj4);
+            }
+            catch
+            {
+                throw new InvalidAgeException("This person doesn't exist");
+            }         
+        }
+        private static void ex_age(Doctor obj4)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
